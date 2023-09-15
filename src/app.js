78fc17e089ee8a91b933c643b7bc60a70e715220
r47orr <razoroller529@gmail.com>
2023-09-14 21:55:05 -0300

    fix for app.js
    * implement all post request listening logic
    
    * implement all embed field creation and filtering
    
    * still webhook requests aren't going through Discord API, though

--- src/app.js
+++ src/app.js
@@ -1,31 +1,41 @@
 const express = require('express')
 const app = express()
 const config = require('./data.json')
-const { fieldBuilderIterator } = require('./utils/processors')
 
 app.use(express.json())
 
-app.post(config.webhook.url, function(req, res) {
+app.post('/', function(req, res) {
 
-    console.log(req.body)
+    let resBody = {
+        username: '',
+        avatar_url: '',
+        embeds: []
+    }
 
-    let resBody = {}
-    let embed = {}
+    const iterable = Object.entries(req.body)
 
-    for (const [key, val] of req.body) {
+    for (const [key, val] of iterable) {
 
         if (!config.fields_names[key] || !config.fields_names[key].embed) continue
 
         let shouldEncode = key == 'error' || key == 'stack' ? true : false
 
-        // do some processing
+        const embedVal = shouldEncode ? `\`\`\`${val}\`\`\`` : val
 
-        embed.append(fieldBuilderIterator(val, 1, shouldEncode))
+        // do some processing
+        resBody.embeds.push({
+            name: config.fields_names[key].title,
+            value: embedVal,
+            inline: false
+        })
     }
 
-    resBody.append(embed)
+    resBody.username = config.webhook.custom_name_or_addon_name || iterable.addon
+    resBody.avatar_url = config.webhook.icon_url_or_path || ''
 
     res.location(config.webhook.url)
+
+    console.log(resBody)
 
     res.send(resBody) // send response to Discord API as a webhook
 
@@ -33,7 +43,5 @@ app.post(config.webhook.url, function(req, res) {
 
 const server = app.listen(3000, () => { //config.port, () => {
 
-    console.log(`Back-end on-line! Alocado com êxito no endereço ${Object.toString(server.address())}`)
+    console.log(`Back-end on-line! Alocado com êxito na porta ${server.address().port}`)
 })
-
-module.exports = server
