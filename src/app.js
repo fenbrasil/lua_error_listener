1c4f8dcab64c22cb9e1fc931af8b1bdc47dbf2dd
r47orr <razoroller529@gmail.com>
2023-09-21 03:37:45 -0300

    fix app.js
    * use built-in NodeJS HTTPS requests module to send Discord API webhook request
    
    * received POST request from Gmod will properly be appended to the *sent* POST message to Discord API
    
    * still need to be refactored and use best coding practices

--- src/app.js
+++ src/app.js
@@ -1,44 +1,108 @@
 const express = require('express')
+const https = require('https')
 const app = express()
 const config = require('./data.json')
 
+function sendHttpResponse(body) {
+
+    const reqBody = JSON.stringify(body)
+
+    console.log(reqBody)
+
+    let req = https.request({
+        host: 'discord.com',
+        path: `/api/webhooks/${config.webhook.url.channel_id}/${config.webhook.url.token}`,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }, function(res) {
+
+        res.setEncoding('utf-8')
+        
+        // console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
+        console.log(`Request status code: ${res.statusCode}`)
+
+        res.on('data', function(d) {
+
+            process.stdout.write(d)
+        })
+    })
+
+    req.on('error', function(e) {
+
+        console.log(`There was an error with the request: ${e.message}`)
+
+        return
+    })
+
+    req.write(reqBody)
+    req.end()
+}
+
 app.use(express.json())
 
-app.post('/', function(req, res) {
+app.post('/', (req, res) => {
 
     let resBody = {
-        username: '',
-        avatar_url: '',
+        // username: '',
+        // avatar_url: '',
         embeds: []
     }
 
+    const timestamp = new Date()
+
     const iterable = Object.entries(req.body)
+
+    // resBody.username = config.webhook.custom_name_or_addon_name || iterable['addon']
+    // resBody.avatar_url = config.webhook.icon_url_or_path || ''
+
+    let embed = {
+        description: 'Erro lua ocorrido no servidor:',
+        color: config.embed.color,
+        fields: [],
+        timestamp: timestamp.toISOString(),
+        footer: {
+            "text": "",
+            "icon_url": ""
+        }
+    }
 
     for (const [key, val] of iterable) {
 
-        if (!config.fields_names[key] || !config.fields_names[key].embed) continue
+        if (!config.embed.fields_names[key] || !config.embed.fields_names[key].type) continue
 
         let shouldEncode = key == 'error' || key == 'stack' ? true : false
 
         const embedVal = shouldEncode ? `\`\`\`${val}\`\`\`` : val
 
         // do some processing
-        resBody.embeds.push({
-            name: config.fields_names[key].title,
+        if (config.embed.fields_names[key].type == 'footer') {
+            
+            embed.footer['text'] = embedVal
+            embed.footer['icon_url'] = config.webhook.icon_url_or_path || ''
+
+            continue
+        }
+        if (config.embed.fields_names[key].type != 'fields') {
+
+            embed[config.embed.fields_names[key].type] = embedVal
+            
+            continue
+        }
+
+        embed.fields.push({
+            name: config.embed.fields_names[key].title,
             value: embedVal,
             inline: false
         })
     }
 
-    resBody.username = config.webhook.custom_name_or_addon_name || iterable.addon
-    resBody.avatar_url = config.webhook.icon_url_or_path || ''
+    resBody.embeds.push(embed)
 
-    res.location(config.webhook.url)
+    sendHttpResponse(resBody)
 
-    console.log(resBody)
-
-    res.send(resBody) // send response to Discord API as a webhook
-
+    res.send()
 })
 
 const server = app.listen(3000, () => { //config.port, () => {
